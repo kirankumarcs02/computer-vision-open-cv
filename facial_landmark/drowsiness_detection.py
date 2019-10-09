0531ae000024f0b79fa4e11b0d7c0446c88ab5dd
kirankumarcs02 <kirankumarcs02@gmail.com>
2019-10-09 19:39:56 +0530

    rename the file

--- facial_landmark/drowsiness_detection.py
+++ facial_landmark/drowsiness_detection.py
@@ -53,8 +53,6 @@ while True:
             if flag >= frame_check:
                 cv2.putText(frame, "****************ALERT!****************", (10, 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                cv2.putText(frame, "****************ALERT!****************", (10, 325),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             # print ("Drowsy")
         elif avg > thresh:
             print("-----------EYE OPEN------------")
